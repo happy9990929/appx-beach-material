605013a9dba7c52ce04f05075118ae2f362dffd6
Yi Wen Wang <41117521+happy9990929@users.noreply.github.com>
2024-08-29 14:40:50 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,8 +2,4 @@ $(function(){
   $("#bar").on("click", () => {
     $("#nav").slideToggle();
   })
-
-  $("#submit").on("click", () => {
-    alert("Sent! Thank you for your subscription!")
-  })
 })
